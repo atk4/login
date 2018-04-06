46ccf94491e8588d81c31edb1a18bc31a05b1d38
Romans Malinovskis <romans@agiletoolkit.org>
2018-04-06 23:41:45 +0100

    magret to use migrator provided by atk4/schema

--- demos/wizard.php
+++ demos/wizard.php
@@ -3,37 +3,13 @@
 include '../vendor/autoload.php';
 include 'db.php';
 
-$app = new \atk4\ui\App('Auth Demo App');
-$app->initLayout('Centered');
- 
+$app = new App('centered');
+
 $app->add(['Header', 'Quickly checking if database is OK']);
-$console = $app->add('Console');
+$console = $app->add('\atk4\schema\MigratorConsole');
 
 $button = $app->add(['Button', '<< Back', 'huge wide blue'])
     ->addStyle('display', 'none')
     ->link(['index']);
 
-$console->set(function($c) use ($button) {
-    $c->output('Connecting..');
-
-    $config = include('config.php');
-
-    $c->output(' .. using DSN '. $config['dsn']);
-    $p = \atk4\data\Persistence::connect($config['dsn']);
-
-    $c->output('Migrating Models');
-
-    foreach(['User'] as $model) {
-        $c->output(' .. '. $model);
-        $model = $c->app->factory($model, null, '\atk4\login\Model');
-        $p->add($model);
-
-        $m = new \atk4\schema\Migration\MySQL($model);
-        $m->migrate();
-    }
-
-    $c->output('Everything looks good!');
-
-    $c->send($button->js()->show());
-});
-
+$console->migrateModels(['\atk4\login\Model\User']);
