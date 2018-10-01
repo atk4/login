d03774dcfd8586a85bdc6dd85014c5d4fd2cd1a4
DarkSide <imants.horsts@inbox.lv>
2018-10-01 10:49:33 +0300

    Only admin should be able to change `is_admin` field, fix #9

--- src/Model/User.php
+++ src/Model/User.php
@@ -16,5 +16,11 @@ class User extends \atk4\data\Model
         $this->addField('email');
         $this->addField('is_admin', ['type'=>'boolean']);
         $this->addField('password', ['\atk4\login\Field\Password']);
+        
+        $this->addHook('afterLoad', function($m){
+            if ($f = $m->hasElement('is_admin')) {
+                $f->read_only = $f->read_only || !$f->get();
+            }
+        });
     }
 }
