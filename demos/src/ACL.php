08417d61257370a36d0776f654efed3f8df40337
DarkSide <imants.horsts@inbox.lv>
2019-09-18 14:50:33 +0300

    make demo config configurable

--- demos/src/ACL.php
+++ demos/src/ACL.php
@@ -1,8 +1,5 @@
 <?php
-
-
 namespace atk4\login\demo;
-
 
 use atk4\data\Model;
 use atk4\data\Persistence;
@@ -10,15 +7,15 @@ use atk4\login\ACL;
 use atk4\login\Model\Admins;
 use atk4\login\Model\User;
 
-class TestACL extends ACL {
-
+class TestACL extends ACL
+{
     protected $permissions = [];
 
     /**
      * Gather permissions of currently logged in users for faster access
      */
-    function cachePermissions() {
-
+    public function cachePermissions()
+    {
         $this->permissions = [
             'admin' => $this->auth->user->loaded() && $this->auth->user['is_admin']
         ];
@@ -27,14 +24,13 @@ class TestACL extends ACL {
     /**
      * Call $app->acl->can('admin'); for example to find out if user is allowed to admin things.
      */
-    function can($feature) {
-
+    public function can($feature)
+    {
         if (!$this->permissions) {
             $this->cachePermissions();
         }
 
         return $this->permissions[$feature];
-
     }
 
 
@@ -42,16 +38,14 @@ class TestACL extends ACL {
      * Will apply per-model modifications (after it's initialized) which will take permissions
      * into account.
      */
-    function applyRestrictions(Persistence $p, Model $m) {
-
+    public function applyRestrictions(Persistence $p, Model $m)
+    {
         if($m instanceof User && !$this->can('admin')) {
-            $m->getElement('email')->read_only = true;
+            $m->getField('email')->read_only = true;
         }
 
         if($m instanceof Admins && !$this->can('admin')) {
             throw Exception();
         }
-
     }
 }
-
--- demos/src/App.php
+++ demos/src/App.php
@@ -1,25 +1,24 @@
 <?php
-
-
 namespace atk4\login\demo;
-
 
 /**
  * Example implementation of your Authenticated application.
  *
  * @package atk4\login\demo
  */
-class App extends \atk4\ui\App {
+class App extends \atk4\ui\App
+{
+    use \atk4\core\ConfigTrait;
+
     public $db;
     public $auth;
     public $title = 'Auth Demo App';
 
-    public $dsn = 'mysql://root:root@localhost/login';
-
-    function __construct($interface = 'front', $no_db_connect = false) {
-
+    public function __construct($interface = 'front', $no_db_connect = false)
+    {
         parent::__construct();
-
+        
+        $this->readConfig(__DIR__.'/../config.php', 'php-inline');
 
         if ($interface == 'admin') {
             $this->initLayout('Admin');
@@ -32,13 +31,13 @@ class App extends \atk4\ui\App {
         }
 
         if (!$no_db_connect) {
-            $this->dbConnect($this->dsn);
+            $this->dbConnect($this->config['dsn']);
         }
 
         $this->authenticate();
     }
 
-    function authenticate()
+    public function authenticate()
     {
         $this->auth = $this->add(new \atk4\login\Auth(['check'=>false]));
         $m = new \atk4\login\Model\User($this->db);
