d68f378a07383c46d89f5eb0baac6043a0fb326f
DarkSide <imants.horsts@inbox.lv>
2020-10-27 01:57:28 +0200

    wtf?

--- demos/_demo-data/create-db.php
+++ demos/_demo-data/create-db.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace atk4\login\demo;
 
-require '../../vendor/autoload.php';
+include '../../vendor/autoload.php';
 
 // CREATE TABLES AND POPULATE DATA ------------------------
-$config = require '../config.php';
+$config = include '../config.php';
 $data = file_get_contents('dump.sql');
 
 $c = \atk4\dsql\Connection::connect($config['dns']);
