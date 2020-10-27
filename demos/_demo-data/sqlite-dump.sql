cfa5ad88936b97a289f4cd71f2ca73755cc50f06
DarkSide <imants.horsts@inbox.lv>
2020-10-27 12:48:29 +0200

    step by step

--- demos/_demo-data/sqlite-dump.sql
+++ demos/_demo-data/sqlite-dump.sql
@@ -1,1 +1,8 @@
-CREATE TABLE "test" ("id" integer PRIMARY KEY, "name" text);
+DROP TABLE IF EXISTS "login_user";
+CREATE TABLE "login_user" (
+  "id" integer PRIMARY KEY,
+  "name" text,
+  "email" text,
+  "password" text,
+  "role_id" integer
+);
