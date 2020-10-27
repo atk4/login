0d5166df674ac76c29303755a708aaf633b5610e
DarkSide <imants.horsts@inbox.lv>
2020-10-27 02:48:32 +0200

    multiline?

--- demos/_demo-data/create-db.php
+++ demos/_demo-data/create-db.php
@@ -8,14 +8,14 @@ require '../../vendor/autoload.php';
 
 // CREATE TABLES AND POPULATE DATA ------------------------
 $config = require '../config.php';
-$data = file_get_contents('dump.sql');
-var_dump($config);
-var_dump($data);
+$data = str_replace(["\n","\r"],'',file_get_contents('dump.sql'));
+//var_dump($config);
+//var_dump($data);
 
 $c = \atk4\dsql\Connection::connect($config['dsn']);
-var_dump($c);
+//var_dump($c);
 $pdo = $c->expr($data)->execute();
-var_dump($pdo);
+//var_dump($pdo);
 
 var_dump($c->expr('SELECT name FROM sqlite_master WHERE type = "table"')->get());
 var_dump($c->expr('select * from login_user')->get());
