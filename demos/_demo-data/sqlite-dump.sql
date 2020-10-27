837410fc33e3a3666f02fd361f208c5d0dc55553
DarkSide <imants.horsts@inbox.lv>
2020-10-27 13:26:11 +0200

    test

--- demos/_demo-data/sqlite-dump.sql
+++ demos/_demo-data/sqlite-dump.sql
@@ -1,8 +1,2 @@
-CREATE TABLE "test1" (
-  "id" integer PRIMARY KEY,
-  "name" text
-);
-CREATE TABLE "test2" (
-  "id" integer PRIMARY KEY,
-  "name" text
-);
+CREATE TABLE "test1" ("id" integer PRIMARY KEY, "name" text)
+CREATE TABLE "test2" ("id" integer PRIMARY KEY, "name" text)
