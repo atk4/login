861aa71953d313924cc2f0ef211875a28333514b
DarkSide <imants.horsts@inbox.lv>
2020-10-27 17:04:11 +0200

    test

--- demos/db.php
+++ demos/db.php
@@ -7,14 +7,21 @@ namespace atk4\login\demo;
 require 'src/App.php';
 require 'src/MigratorConsole.php';
 
-if (file_exists('config.php')) {
-    $config = require 'config.php';
-    $db = new \atk4\data\Persistence\Sql($config['dsn']);
-} else {
-    $sqliteFile = __DIR__ . '/_demo-data/db.sqlite';
-    if (!file_exists($sqliteFile)) {
-        throw new \Exception('Sqlite database does not exist, create it first.');
-    }
-    $db = new \atk4\data\Persistence\Sql('sqlite:' . $sqliteFile);
-    unset($sqliteFile);
+// creates config file if possible
+$sqliteFile = __DIR__ . '/_demo-data/db.sqlite';
+if (!file_exists('config.php') && file_exists($sqliteFile)) {
+    $string_config = "<?php\n\n" .
+        "declare(strict_types=1);\n\n" .
+        "return [\n" .
+        "    'dsn'=>'sqlite:" . $sqliteFile . "'\n" .
+        "];\n";
+    file_put_contents('config.php', $string_config);
 }
+
+// connect database if possible
+if (!file_exists('config.php')) {
+    throw new \Exception('Sqlite database or config.php file does not exist.');
+}
+
+$config = require 'config.php';
+$db = new \atk4\data\Persistence\Sql($config['dsn']);
