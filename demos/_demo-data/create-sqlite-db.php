260e6852fb5831a92662370b3319a455888303e3
Francesco Danti <fdanti@gmail.com>
2021-04-23 10:01:49 +0200

    Fix database creation

--- demos/_demo-data/create-sqlite-db.php
+++ demos/_demo-data/create-sqlite-db.php
@@ -6,49 +6,15 @@ namespace Atk4\Login\Demos;
 
 use Atk4\Data\Model;
 
-require_once __DIR__ . '/../init.php';
+include __DIR__ . '/../../vendor/autoload.php';
 
-$sqliteFile = __DIR__ . '/db.sqlite';
+$sqliteFile = __DIR__ . '/../data/db.sqlite';
 if (file_exists($sqliteFile)) {
     unlink($sqliteFile);
 }
 
 $persistence = new \Atk4\Data\Persistence\Sql('sqlite:' . $sqliteFile);
-
-class ImportModelWithPrefixedFields extends Model
-{
-    private function prefixFieldName(string $fieldName, bool $forActualName = false): string
-    {
-        return 'atk_' . ($forActualName ? 'a' : '') . 'fp_' . $this->table . '__' . $fieldName;
-    }
-
-    public function addField($name, $seed = []): \Atk4\Data\Field
-    {
-        if ($name === 'id') {
-            $this->id_field = $this->prefixFieldName($name);
-        }
-
-        $seed = \Atk4\Core\Factory::mergeSeeds($seed, [
-            'actual' => $this->prefixFieldName($name, true),
-        ]);
-
-        return parent::addField($this->prefixFieldName($name), $seed);
-    }
-
-    public function import(array $rowsMulti)
-    {
-        return parent::import(array_map(function (array $rows): array {
-            $rowsPrefixed = [];
-            foreach ($rows as $k => $v) {
-                $rowsPrefixed[$this->prefixFieldName($k)] = $v;
-            }
-
-            return $rowsPrefixed;
-        }, $rowsMulti));
-    }
-}
-
-$model = new ImportModelWithPrefixedFields($persistence, ['table' => 'login_user']);
+$model = new \Atk4\Data\Model($persistence, ['table' => 'login_user']);
 $model->addField('name', ['type' => 'string']);
 $model->addField('email', ['type' => 'string']);
 $model->addField('password', ['type' => 'string']);
@@ -59,7 +25,7 @@ $model->import([
     2 => ['id' => 2, 'name' => 'Administrator', 'email' => 'admin', 'password' => '$2y$10$p34ciRcg9GZyxukkLIaEnenGBao79fTFa4tFSrl7FvqrxnmEGlD4O', 'role_id' => 2], // admin/admin
 ]);
 
-$model = new ImportModelWithPrefixedFields($persistence, ['table' => 'login_role']);
+$model = new \Atk4\Data\Model($persistence, ['table' => 'login_role']);
 $model->addField('name', ['type' => 'string']);
 (new \Atk4\Schema\Migration($model))->dropIfExists()->create();
 $model->import([
@@ -67,7 +33,7 @@ $model->import([
     2 => ['id' => 2, 'name' => 'Admin Role'],
 ]);
 
-$model = new ImportModelWithPrefixedFields($persistence, ['table' => 'login_access_rule']);
+$model = new \Atk4\Data\Model($persistence, ['table' => 'login_access_role']);
 $model->addField('role_id', ['type' => 'integer']);
 $model->addField('model', ['type' => 'string']);
 $model->addField('all_visible', ['type' => 'boolean']);
@@ -77,7 +43,6 @@ $model->addField('editable_fields', ['type' => 'boolean']);
 $model->addField('all_actions', ['type' => 'boolean']);
 $model->addField('actions', ['type' => 'boolean']);
 $model->addField('conditions', ['type' => 'boolean']);
-
 
 (new \Atk4\Schema\Migration($model))->dropIfExists()->create();
 $model->import([
