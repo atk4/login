d812955804fa04cbd2ce0798250c7e894f2a2c44
Michael Voříšek <mvorisek@mvorisek.cz>
2021-10-19 02:39:56 +0200

    initAcl must be currently called manually

--- demos/admin-users.php
+++ demos/admin-users.php
@@ -11,5 +11,8 @@ use Atk4\Ui\Header;
 /** @var App $app */
 require_once __DIR__ . '/init-app.php';
 
+// switch on ACL so it will be applied for all models added to persistence from now on
+$app->initAcl();
+
 Header::addTo($app)->set('Users');
 UserAdmin::addTo($app)->setModel(new User($app->db));
