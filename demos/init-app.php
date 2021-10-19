ca243718509a772e3ab457da1855278477553eba
Michael Voříšek <mvorisek@mvorisek.cz>
2021-10-19 11:40:20 +0200

    add webserver coverage

--- demos/init-app.php
+++ demos/init-app.php
@@ -8,7 +8,20 @@ date_default_timezone_set('UTC');
 
 require_once __DIR__ . '/init-autoloader.php';
 
+// collect coverage for HTTP tests 1/2
+if (file_exists(__DIR__ . '/CoverageUtil.php') && !class_exists(\PHPUnit\Framework\TestCase::class, false)) {
+    require_once __DIR__ . '/CoverageUtil.php';
+    \CoverageUtil::start();
+}
+
 $app = new App();
+
+// collect coverage for HTTP tests 2/2
+if (file_exists(__DIR__ . '/CoverageUtil.php') && !class_exists(\PHPUnit\Framework\TestCase::class, false)) {
+    $app->onHook(\Atk4\Ui\App::HOOK_BEFORE_EXIT, function () {
+        \CoverageUtil::saveData();
+    });
+}
 
 try {
     /** @var \Atk4\Data\Persistence\Sql $db */
