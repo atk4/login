cbc60d80886e7337ff85a4ce6e074668d4bd5929
Michael Voříšek <mvorisek@mvorisek.cz>
2021-11-24 12:55:16 +0100

    Adjust to renamed Model::loaded method (#82)

--- src/Acl.php
+++ src/Acl.php
@@ -30,7 +30,7 @@ class Acl
         /** @var \Atk4\Login\Model\User */
         $user = $this->auth->user;
 
-        if (!$user->loaded()) {
+        if (!$user->isLoaded()) {
             // user is not logged in - let's force him to do so. Alternative is to throw exception, but that's ugly.
             $this->auth->check();
             //throw new Exception('User should be logged in!');
