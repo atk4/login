7c11158057fc485be898e79ee56be53e102defac
Michael Voříšek <mvorisek@mvorisek.cz>
2023-02-10 13:34:05 +0100

    getPersistence() can be called on model only

--- src/Auth.php
+++ src/Auth.php
@@ -186,7 +186,7 @@ class Auth
         // first logout
         $this->logout();
 
-        $userModel = new $this->user($this->user->getPersistence());
+        $userModel = new $this->user($this->user->getModel()->getPersistence());
 
         $userEntity = $userModel->tryLoadBy($this->fieldLogin, $email);
         if ($userEntity !== null) {
@@ -233,7 +233,7 @@ class Auth
      */
     public function setAcl(Acl $acl, Persistence $persistence = null)
     {
-        $persistence ??= $this->user->getPersistence();
+        $persistence ??= $this->user->getModel()->getPersistence();
         $acl->auth = $this;
         $acl->applyRestrictions($this->user);
 
