824233cf98b6c8de5b25ae95bfd0a8f5a1db3dbd
DarkSide <imants.horsts@inbox.lv>
2024-03-21 16:21:11 +0200

    fix tests

--- tests/GenericTestCase.php
+++ tests/GenericTestCase.php
@@ -13,6 +13,17 @@ use Atk4\Ui\App;
 
 abstract class GenericTestCase extends BaseTestCase
 {
+    use CreateAppTrait;
+
+    /**
+     * Put model table names here. Needed for DB import.
+     */
+    private static array $modelTables = [
+        Role::class => 'login_role',
+        User::class => 'login_user',
+        AccessRule::class => 'login_access_rule',
+    ];
+
     protected function tearDown(): void
     {
         \Closure::bind(static function () {
@@ -24,7 +35,7 @@ abstract class GenericTestCase extends BaseTestCase
 
     protected function createAppForSession(): App
     {
-        $app = new App([
+        $app = $this->createApp([
             'catchExceptions' => false,
             'alwaysRun' => false,
         ]);
@@ -32,6 +43,7 @@ abstract class GenericTestCase extends BaseTestCase
         $app->session = new class() extends App\SessionManager {
             /** @var array<string, mixed> */
             private $data = [];
+
             /** @var bool */
             private $isActive = false;
 
@@ -65,15 +77,15 @@ abstract class GenericTestCase extends BaseTestCase
     protected function setupDefaultDb(): void
     {
         $this->setDb([
-            self::getTableByStandardModelClass(Role::class) => [
+            self::getTableByModelClass(Role::class) => [
                 ['id' => 1, 'name' => 'User Role'],
                 ['id' => 2, 'name' => 'Admin Role'],
             ],
-            self::getTableByStandardModelClass(User::class) => [
+            self::getTableByModelClass(User::class) => [
                 ['id' => 1, 'name' => 'Standard User', 'email' => 'user', 'password' => '$2y$10$BwEhcP8f15yOexf077VTHOnySn/mit49ZhpfeBkORQhrsmHr4U6Qy' /* user */, 'role_id' => 1],
                 ['id' => 2, 'name' => 'Administrator', 'email' => 'admin', 'password' => '$2y$10$p34ciRcg9GZyxukkLIaEnenGBao79fTFa4tFSrl7FvqrxnmEGlD4O' /* admin */, 'role_id' => 2],
             ],
-            self::getTableByStandardModelClass(AccessRule::class) => [
+            self::getTableByModelClass(AccessRule::class) => [
                 ['id' => 1, 'role_id' => 1, 'model' => User::class, 'all_visible' => 1, 'visible_fields' => null, 'all_editable' => 0, 'editable_fields' => 'vat_number,active', 'all_actions' => 1, 'actions' => null, 'conditions' => null],
                 ['id' => 2, 'role_id' => 2, 'model' => User::class, 'all_visible' => 1, 'visible_fields' => null, 'all_editable' => 0, 'editable_fields' => null, 'all_actions' => 1, 'actions' => null, 'conditions' => null],
                 ['id' => 3, 'role_id' => 2, 'model' => Role::class, 'all_visible' => 1, 'visible_fields' => null, 'all_editable' => 1, 'editable_fields' => null, 'all_actions' => 1, 'actions' => null, 'conditions' => null],
@@ -81,90 +93,28 @@ abstract class GenericTestCase extends BaseTestCase
         ]);
     }
 
-    private static function getTableByStandardModelClass(string $modelClass): string
+    private static function getTableByModelClass(string $modelClass): string
     {
-        return [
-            Role::class => 'unit_role',
-            User::class => 'unit_user',
-            AccessRule::class => 'unit_access_rule',
-        ][$modelClass];
+        return self::$modelTables[$modelClass];
     }
 
-    public static function replaceTableAndModelsInRefs(Model $model): void
+    private function createModel(string $modelClass): Model
     {
-        $model->table = self::getTableByStandardModelClass(get_parent_class($model)); // @phpstan-ignore-line https://github.com/phpstan/phpstan/issues/4302
-        foreach ($model->getReferences() as $k => $r) {
-            if ($r->model instanceof \Closure) {
-                if ($model instanceof User && $k === 'AccessRules') { // safe Closure, reference is build using ->ref()
-                    continue;
-                }
-            }
-
-            $r->model[0] = self::getClassByStandardModelClass($r->model[0]);
-        }
-    }
-
-    private static function getClassByStandardModelClass(string $modelClass): string
-    {
-        return get_class(([
-            Role::class => function () {
-                return new class() extends Role {
-                    public $table = '';
-
-                    protected function init(): void
-                    {
-                        parent::init();
-
-                        GenericTestCase::replaceTableAndModelsInRefs($this);
-                    }
-                };
-            },
-            User::class => function () {
-                return new class() extends User {
-                    public $table = '';
-
-                    protected function init(): void
-                    {
-                        parent::init();
-
-                        GenericTestCase::replaceTableAndModelsInRefs($this);
-                    }
-                };
-            },
-            AccessRule::class => function () {
-                return new class() extends AccessRule {
-                    public $table = '';
-
-                    protected function init(): void
-                    {
-                        parent::init();
-
-                        GenericTestCase::replaceTableAndModelsInRefs($this);
-                    }
-                };
-            },
-        ][$modelClass])());
-    }
-
-    private function createModelByStandardModelClass(string $modelClass): Model
-    {
-        $class = self::getClassByStandardModelClass($modelClass);
-
-        return new $class($this->db);
+        return new $modelClass($this->db);
     }
 
     protected function createRoleModel(): Role
     {
-        return $this->createModelByStandardModelClass(Role::class); // @phpstan-ignore-line
+        return $this->createModel(Role::class); // @phpstan-ignore-line
     }
 
     protected function createUserModel(): User
     {
-        return $this->createModelByStandardModelClass(User::class); // @phpstan-ignore-line
+        return $this->createModel(User::class); // @phpstan-ignore-line
     }
 
     protected function createAccessRuleModel(): AccessRule
     {
-        return $this->createModelByStandardModelClass(AccessRule::class); // @phpstan-ignore-line
+        return $this->createModel(AccessRule::class); // @phpstan-ignore-line
     }
 }
