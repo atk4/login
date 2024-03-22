fd3e931d986f938b68c484ff8c42fe2785218ba9
DarkSide <imants.horsts@inbox.lv>
2024-03-22 17:30:29 +0200

    set to be compatible with 5.0.0

--- src/Feature/SetupUserModelTrait.php
+++ src/Feature/SetupUserModelTrait.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Atk4\Login\Feature;
 
-use Atk4\Data\Persistence;
+// use Atk4\Data\Persistence; // for 5.1.0 compatibility
 
 trait SetupUserModelTrait
 {
@@ -21,8 +21,12 @@ trait SetupUserModelTrait
         // all AccessRules for all user roles
         // @TODO in future when there can be multiple, then merge them together
         $this->hasMany('AccessRules', [
-            'model' => function (Persistence $p, array $defaults = []) {
-                return $this->ref('role_id')->ref('AccessRules');
+            // for 5.1.0 compatibility
+            // 'model' => function (Persistence $p, array $defaults = []) {
+            //    return $this->ref('role_id')->ref('AccessRules');
+            // }
+            'model' => static function ($m) {
+                return $m->ref('role_id')->ref('AccessRules');
             },
             'ourField' => 'role_id',
             'theirField' => 'role_id',
