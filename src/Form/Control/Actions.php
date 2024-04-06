da0be85d3a26214ffbc69c27ef5d1235adb54735
DarkSide <imants.horsts@inbox.lv>
2024-04-06 19:21:52 +0300

    Remove unused methods

--- src/Form/Control/Actions.php
+++ src/Form/Control/Actions.php
@@ -4,25 +4,8 @@ declare(strict_types=1);
 
 namespace Atk4\Login\Form\Control;
 
-use Atk4\Data\Field;
-use Atk4\Data\Model;
-
 class Actions extends GenericDropdown
 {
-    #[\Override]
-    public function setModel(Model $model): void
-    {
-        $this->renderRowFunction = static function (Field $field) {
-            return [
-                'value' => $field->shortName,
-                'title' => $field->caption ?? $field->shortName,
-                'icon' => $field->ui['icon'] ?? null,
-            ];
-        };
-
-        parent::setModel($model);
-    }
-
     #[\Override]
     protected function renderView(): void
     {
