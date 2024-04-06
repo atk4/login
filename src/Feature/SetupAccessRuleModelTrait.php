b3cbaec5e7ad5ee2f479e2b6c134f9bea456b1bf
DarkSide <imants.horsts@inbox.lv>
2024-04-06 18:07:12 +0300

    add comment

--- src/Feature/SetupAccessRuleModelTrait.php
+++ src/Feature/SetupAccessRuleModelTrait.php
@@ -38,6 +38,7 @@ trait SetupAccessRuleModelTrait
 
         // cleanup data
         $this->onHook(Model::HOOK_BEFORE_SAVE, static function (self $m) {
+            // @todo this is not always right way to normalize data
             if ($m->get('all_visible')) {
                 $m->setNull('visible_fields');
             }
