efbcae64d8a01f7ec06a416f41f5e4be35ec15cf
DarkSide <imants.horsts@inbox.lv>
2024-04-16 16:57:28 +0300

    If you define rules on default Atk4\Data\Model class, then it do not have $table set and will fail

--- src/Form/Control/GenericDropdown.php
+++ src/Form/Control/GenericDropdown.php
@@ -33,7 +33,7 @@ abstract class GenericDropdown extends Form\Control\Dropdown
             return null;
         }
 
-        $model = new $class($this->form->entity->getModel()->getPersistence());
+        $model = new $class($this->form->entity->getModel()->getPersistence(), ['table' => '']);
         if (!$model instanceof Model) {
             throw new Exception('Class must be instance of ' . Model::class);
         }
